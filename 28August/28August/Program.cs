e3261c1efe500465fe440c8583078a72614d0938
Iksniratap <patarinski@gmail.com>
2017-03-28 16:46:52 +0300

    Add files via upload

---
+++ 28August/28August/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28August
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double wage = double.Parse(Console.ReadLine());
+            double exchangeRate = double.Parse(Console.ReadLine());
+            wage *= exchangeRate * days;
+            double fullWage = wage * 14.5;
+            fullWage *= 0.75;
+            double dailyWage = fullWage / 365;
+            Console.WriteLine("{0:F2}", dailyWage);
+        }
+    }
+}
---
+++ 28August/Firma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _02.Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var neededHours = int.Parse(Console.ReadLine());
+            var days = int.Parse(Console.ReadLine());
+            var overtimeWorkers = int.Parse(Console.ReadLine());
+            var realDays = days * 0.9;
+            var workingHours = Math.Floor(realDays * 8);
+            var overtime = overtimeWorkers * 2 * days;
+            var totalWorkingHours = workingHours + overtime;
+
+            if (totalWorkingHours >= neededHours)
+            {
+                var leftHours = totalWorkingHours - neededHours;
+                Console.WriteLine("Yes!{0} hours left.", leftHours);
+            }
+            else
+            {
+                var insufficientHours = neededHours - totalWorkingHours;
+                Console.WriteLine("Not enough time!{0} hours needed.", insufficientHours);
+            }
+        }
+    }
+}
